0bf4c5c52c36a5f3a99083246b0f9133f5e672ba
Alexander Sokol <reb2020@gmail.com>
2020-04-03 23:45:27 +0300

    update

--- compiled/builder/helpers/alias.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function () {
-  return typeof this.state.alias !== 'undefined' ? this.state.alias : this.state.table;
-};
